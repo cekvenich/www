aee3a88bbf96bbfa9bdab82babacdad4f74bb54b
puppetmaster3 <vicmasons@gmail.com>
2017-03-28 19:11:04 -0400

    S

--- webroot/_js/main.js
+++ webroot/_js/main.js
@@ -1,18 +1,15 @@
 'use strict'
 
-loadjs.ready(['dependencyNotChrome', 'keyLibs'], {// loaded setup libs
+loadjs.ready(['dependencyIE', 'keyLibs'], {// loaded setup libs
 	success: function(){
-	//console.log('loading libs')
 	loadjs([
-		'//d2wy8f7a9ursnm.cloudfront.net/bugsnag-3.min.js'
-		,'/_js/libJs/jquery.smoothState.js'
+		'https://cdn.rawgit.com/topseed/topseed-npm/master/browserSide/deps/jquery.smoothState.js',
 
 		//dbind:
 		,'/_js/libJs/jquery.jsForm.min.js'
 
 		,'/_js/libJs/jquery.fullpage.min.js'
 		,'//cdn.jsdelivr.net/jquery.transit/0.9.12/jquery.transit.min.js'
-		//'//cdn.jsdelivr.net/jquery.cookie/1.4.1/jquery.cookie.min.js'
 		//,'/_js/libJs/zingtouch.min.js'
 
 		,'//cdn.radiantmediatechs.com/rmp/v4/latest/js/rmp.min.js'
@@ -31,22 +28,7 @@ loadjs.ready(['dependencyNotChrome', 'keyLibs'], {// loaded setup libs
 
 //====================================================================
 function startApp(){
-	
-	function toggleSide(){
-		console.log('tog')
-		var $sidedrawer=$('#sidedrawer')
-		$sidedrawer.toggleClass('active')
-	}
-	function initSideDraw() {
-		setTimeout(function(){
-			$('#brand').unbind('click').click(toggleSide)
-			$('#sidedrawer').unbind('click').click(toggleSide)
-			console.log('initSD')
 
-		}, 200)
-	}
-	initSideDraw()
-	// READY ///////////////////////////////////////////////////////////
 	A.loaded=true
 	A.act(A.LOADED)
 
@@ -76,7 +58,6 @@ function startApp(){
 				$('#content-wrapper').replaceWith($newContent.last())
 				//$container.html($newContent)
 				$('content-wrapper').fadeTo(1000/30,1)
-				initSideDraw()
 
 				A.act(A.PAGE)// main action
 				A.inAction= false
