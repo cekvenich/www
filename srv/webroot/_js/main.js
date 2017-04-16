21a4c0c69f82b5065143106b5ca609d87550809a
puppetmaster3 <vicmasons@gmail.com>
2017-04-16 15:17:44 -0400

    d

--- srv/webroot/_js/main.js
+++ srv/webroot/_js/main.js
@@ -1,78 +1,56 @@
 'use strict'
-
 loadjs.ready(['dependencyIE', 'keyLibs'], {// loaded setup libs
 	success: function(){
-	loadjs([
-		'https://cdn.rawgit.com/topseed/topseed-npm/master/v1/deps/jquery.smoothState.js'
-		, 'https://cdn.rawgit.com/topseed/topseed-npm/master/v1/deps/riot_compiler.min.js'
 
-		//dbind:
-		,'/_js/libJs/jquery.jsForm.min.js'
+		loadjs.done('ready') // page ready
 
-		,'/_js/libJs/jquery.fullpage.min.js'
-		,'//cdn.jsdelivr.net/jquery.transit/0.9.12/jquery.transit.min.js'
+		loadjs([
+			
+			'https://cdn.rawgit.com/topseed/topseed-npm/master/v1/deps/riot_compiler.min.js'
+			,'//cdn.jsdelivr.net/jquery.transit/0.9.12/jquery.transit.min.js'
 
-		,'//cdn.radiantmediatechs.com/rmp/v4/latest/js/rmp.min.js'
-		, '/_js/split.js'
+			,'//cdn.radiantmediatechs.com/rmp/v4/latest/js/rmp.min.js'
+			, '/_js/split.js'
 
-		], { success: function(){
-			console.log('loaded libs')
-			//Bugsnag.apiKey = 'd69b92d5cad0a35d9ae02d108b071688'
-			//Bugsnag.notify('ErrorName', 'Test Error')
+			//images
+			,'/_js/libJs/jquery.fullpage.min.css'
+			,'/_js/libJs/jquery.fullpage.min.js'
 
-			startApp()
-		}
+			//data
+			,'/_js/libJs/jquery.jsForm.min.js'
+
+			], { success: function(){
+				console.log('loaded libs')
+				startApp()
+			}
 	})//loadjs
 	}//suc
 })
 
-//==========================================================
 function startApp(){
+	// READY ///////////////////////////////////////////////////////////
+	loadjs.done('main')
 
-	A.loaded=true
-	A.act(A.LOADED)
+	sP.ScontentID ='#content-wrapper'
+	sP.smoothPg.add(function(typ, $new, delta, $html) {
+		console.log(typ)
 
-	//>SS==================================================
-	var ssoptions={
-		debug: true,
-		prefetch: true,
-		cacheLength: 2,
-		repeatDelay: 450,
+		if(sP.PRE==typ)  {//start
+			console.log($new)
+			pgSplit($('#content-wrapper'), 450 )
+			//$('#content-wrapper').fadeTo(100,.2)
 
-		onStart: {
-			duration: 0, 
-			render: function (url, $container)  {
-				console.log('-> ')
-				A.act(A.PRE) //action
-				A.inAction=true
+		}
+		if(sP.PAGE==typ)  {//ready
+			$(sP.ScontentID).html($new)
+			//$('#content-wrapper').fadeTo(100,1)
 
-				pgSplit($('#content-wrapper'), 450 )
-				$('#content-wrapper').fadeTo(1000/60,.2)
+		}
 
-			}//r
-		},//onS
-		onReady: {
-			duration: 0,
-			render: function ($container, $newContent) {
-				$('#content-wrapper').replaceWith($newContent)
-				//$container.html($newContent)
-				$('content-wrapper').fadeTo(1000/30,1)
+	})
 
-				A.act(A.PAGE)// main action
-				A.inAction= false
-				console.log('% <-')
-			}//ren
-		}//ready()
-	}//sso
-	
-	var smoothState = $('#ss1').smoothState(ssoptions)
-
-	//setupFlick()
-
-	var endTime = (new Date()).getTime() - _loadStarted
-	console.log('load time ' + endTime)
 }//startApp()
-//==========================================================
+
 
 function setupFlick() {
 
