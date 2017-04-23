57e7b22d7db38bd20f41fe2b122a2d452cbacf76
puppetmaster3 <vicmasons@gmail.com>
2017-04-23 18:30:23 -0400

    d

--- wsrv/index.js
+++ wsrv/index.js
@@ -20,28 +20,6 @@ debug('oh hi')
 const Decider = require('./utils/Decider')
 const contPg = require('./sroute/contPg')
 
-// ###################### dynamic data for some pgs here:
-server.get('/post/amp0/', function (req, res) {
-	const containsWWWW = (req.subdomains.indexOf('www') > -1) //for subdomain
-	const isWWWW = (req.query.w == '1') || containsWWWW
-
-	if(isWWWW) //is it SPA/www? 
-		fs.readFile('./www/post/amp0/spa.html', 'utf8', function(err, data) {
-			res.send(data)
-		})
-	else { //AMP
-		let _people = {people: [{name: 'Jim'}, {name: 'Pedro'}] }
-		fs.readFile('./www/post/amp0/amp.html', 'utf8', function(err, data) {
-			if (err) throw err
-			// js render
-			let tmpl = jsrender.templates(data)
-			let html2 = tmpl.render( _people )
-			res.send(html2)
-		})// readfile
-	} //else
-
-})//()
-
 //front route: ////////////////
 server.use('/contPg', contPg) 
 
