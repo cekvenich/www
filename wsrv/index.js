faf53c8594dc34e5e7a81284527c994b6a523a85
Wolfgang <wgehner@gmail.com>
2017-05-12 05:31:43 -0700

    Cleanup CSS link, fix index.js ServerConfig.PORt

--- wsrv/index.js
+++ wsrv/index.js
@@ -38,7 +38,7 @@ server.listen(ServerConfig.WWW_PORT, '0.0.0.0', function() {
 	console.log('Press Ctrl+C to quit.')
 })
 
-server.listen(ServerConfig.AMP_PORT2, '0.0.0.0', function() {
+server.listen(ServerConfig.AMP_PORT, '0.0.0.0', function() {
 	console.log('App listening on port ' +ServerConfig.AMP_PORT)
 	console.log('Press Ctrl+C to quit.')
 })
