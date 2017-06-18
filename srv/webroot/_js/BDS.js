8adae559aa628214ff67aebef601d21071ea0274
puppetmaster3 <vicmasons@gmail.com>
2017-06-18 06:54:39 -0400

    trying to finish testing

--- srv/webroot/_js/BDS.js
+++ srv/webroot/_js/BDS.js
@@ -1,16 +1,15 @@
 'use strict'
-class BDS {
 
-	static fetch(fetch_,ROOT_, url_, data_, jtoken ) {
-		console.log('fetching ', url_, data_, jtoken)
+var BDS = Class.extend({ //IE11-compatible base class for Data Access Object
+
+	 _fetch: function(fetch_,ROOT_, url_, data_) { //static
+		//var xjt_ = Cookies.get(BDS.XJT)
+		//var xb_  = Cookies.get(BDS.XBASIC)
+		console.log('fetching ', url_)
 		return fetch_(ROOT_ + url_ , { //1 call
 				method: 'post'
 				, headers: {
 					'Content-Type': 'application/json'
-					,'X-JToken' : JSON.stringify(jtoken)
-					,'Accept':'application/json'
-					, credentials: 'same-origin' //res.cookie returned
-
 				}
 				, body: JSON.stringify(data_)
 			}).then(function(response) { //2 returns a promise
@@ -25,7 +24,7 @@ class BDS {
 			})
 	}//_()
 
-} // class
+}) //'class'
 
 // for node:
 if (typeof module !== 'undefined' && typeof module.exports !== 'undefined')
